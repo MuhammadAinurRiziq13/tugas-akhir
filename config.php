e3dfa729d007645e1421bc14d337760c55f2ae62
pdh-24 <131939612+pdh-24@users.noreply.github.com>
2023-11-30 08:21:36 +0700

    Update config.php
    Modifikasi config.php.

--- config.php
+++ config.php
@@ -7,10 +7,21 @@ class Database {
   public $conn;
 
   public function __construct() {
-      $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
-      if ($this->conn->connect_error) {
-          die("Connection failed: " . $this->conn->connect_error);
-      }
+    $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+    if ($this->conn->connect_error) {
+      die("Connection failed: " . $this->conn->connect_error);
+    }
+    $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
+
+    if (mysqli_connect_errno()) {
+      die("Koneksi database gagal: " . mysqli_connect_error());
+    }
+  }
+  public function executeQuery($query) {
+    // $result = mysqli_query($this->conn, $query);
+    $result = $this->conn->query($query);
+    return $result;
   }
 }
+$koneksi = new Database();
 ?>
