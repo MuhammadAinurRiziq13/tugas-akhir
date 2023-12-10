49c5363a74da1dbc8fb7595b6cb8c4c99cd79e41
Muhammad Ainur Riziq <riziqainur13@gmail.com>
2023-12-10 18:39:54 +0700

    transaksi done

--- admin/template/transaksi.php
+++ admin/template/transaksi.php
@@ -1,6 +1,7 @@
 <?php
     require_once 'config/config.php'; // Pastikan file Database.php sudah di-include
     require_once 'classes/Barang.php'; // Pastikan file Item.php sudah di-include
+    require_once 'classes/Transaksi.php'; // Pastikan file Item.php sudah di-include
 
     // Membuat instance dari class Database
     $database = new Database();
@@ -8,6 +9,7 @@
 
     // Membuat instance dari class Item
     $barang = new Barang($conn);
+    $transaksi = new Transaksi($conn);
 ?>
 <?php
   require "classes/DetailTransaksi.php";
@@ -58,15 +60,23 @@
 
                 $no = 1;
                 foreach ($dataBarang as $item) {
-                  echo '<div class="kartu rounded-4 pt-2 bg-white d-flex align-items-center flex-column" style="width:18%">';          
+                  echo '<div class="kartu rounded-4 pt-2 bg-white d-flex align-items-center flex-column" style="width:18%">';
                   echo '<img src="uploads/' . $item['gambar'] . '" class="rounded-4 kartu-image" style="width: 7rem; height: 4rem" />';
-                    echo '<div class="kartu-body pt-3 d-flex align-items-center flex-column">';
-                      echo '<h5 class="kartu-title h6 fw-bold">'  . $item['nama_barang'] . '</h5>';
-                      echo '<p class="kartu-text mb-2 fs-6">' . number_format($item['harga_jual']) . '</p>';
-                      echo '<a href="#" class="btn bg-dongker py-0 rounded-3 mb-2 text-white add-transaksi">Add</a>';
-                    echo '</div>
-                  </div>';                
-                }    
+                  echo '<div class="kartu-body pt-3 d-flex align-items-center flex-column">';
+                  echo '<h5 class="kartu-title h6 fw-bold">'  . $item['nama_barang'] . '</h5>';
+                  echo '<p class="kartu-text mb-2 fs-6">' . number_format($item['harga_jual']) . '</p>';
+                  echo '<form method="POST" action="admin/fungsi/addCart.php" class="add-to-cart-form">';
+                  echo '<input type="hidden" name="id_barang" value="' . $item['id_barang'] . '">';
+                  echo '<div class="items-kuantiti d-flex align-items-end gap-1 ms-4">';
+                  echo '<button type="button" class="min-items">-</button>';
+                  echo '<input type="text" min="1" max="5" value="1" name="quantity_' . $item['id_barang'] . '" class="quantity-input px-2" style="width: 2rem; font-size: 0.8rem" />';
+                  echo '<button type="button" class="plus-items">+</button>';
+                  echo '</div>';
+                  echo '<button type="submit" name="add_to_cart" class="btn bg-dongker py-0 rounded-3 mb-2 text-white add-transaksi mt-2" style="width:8rem">Add</button>';
+                  echo '</form>';
+                  echo '</div> </div>';
+                }
+              
               ?>
             </div>
           </div>
@@ -78,29 +88,59 @@
               <p class="date-menu">01/Sep/2023</p>
             </div>
             <div class="items pt-3" style="height: 72%">
-              <h5 class="fw-bold mb-1">Items</h5>
-                <div class="items-wrap mb-0" style="height: 20rem">
-                  <!-- <div class="list-items-order d-flex border-bot py-2">
-                    <img src="assets/image/nasgor2.png" alt="order items" style="height: 3rem" />
-                    <div class="inner-items-order ms-1 d-flex flex-column justify-content-center">
-                      <p class="items-order-title fw-bold mb-1">Nasgor Biasa</p>
-                      <p class="items-order-harga mb-1">Rp. 10.000</p>
-                    </div>
-                    <div class="items-kuantiti d-flex align-items-end gap-1 ms-4 pe-2">
-                      <button type="button" class="min-items">-</button>
-                      <input type="text" min="1" max="5" value="1" name="quantity" id="quantity" style="width: 2rem; font-size: 0.8rem" class="px-2" value="1"/>
-                      <button type="button" class="plus-items">+</button>
-                    </div>
-                  </div> -->
-                </div>
+              <div class="title d-flex align-items-center justify-content-between">
+                <h5 class="fw-bold mb-1">Items</h5>
+                <!-- <button type="submit" name="delete-cart" class="remove-items btn bg-dongker p-0 text-white" style="height: 1.5rem; width: 4rem; font-size: .7rem">Reset</button> -->
+                <form method="POST" action="admin/fungsi/deleteCart.php">
+                  <button type="submit" name="clear_cart" class="p-0 btn bg-dongker text-white" style="height: 1.5rem; width: 4rem; font-size: .8rem">Reset</button>
+                </form>
+              </div>
+              <div class="items-wrap mb-0" style="height: 20rem">
+              <?php
+                  $totalQuantity = 0;
+
+                  // Calculate total price using the function
+                  $totalPrice = $transaksi->calculateTotalPrice($_SESSION["cart"], $barang);
+
+                  // Loop untuk menampilkan item di keranjang
+                  if (isset($_SESSION["cart"]) && !empty($_SESSION["cart"])) {
+                    foreach ($_SESSION["cart"] as $cartItem) {
+                        $id_barang = $cartItem["id_barang"];
+                        $itemQuantity = $cartItem["quantity_" . $id_barang];
+
+                        // Query ke database untuk mendapatkan informasi barang
+                        $item = $barang->getBarangById($id_barang);
+                        ?>
+                        <div class="list-items-order d-flex border-bot py-2">
+                          <img src="uploads/<?php echo $item['gambar']; ?>" alt="order items" style="height: 50px;width:70px;" />
+                          <div class="inner-items-order ms-1 d-flex flex-column justify-content-center">
+                              <p class="items-order-title fw-bold mb-1"><?php echo $item['nama_barang']; ?></p>
+                              <p class="items-order-harga mb-1">Rp. <?php echo number_format($item['harga_jual']); ?></p>
+                          </div>
+                          <div class="items-kuantiti d-flex flex-column justify-content-between align-items-end gap-1 ms-4">
+                              <form method="POST" action="admin/fungsi/deleteCart.php">
+                                <input type="hidden" name="id_barang" value="<?php echo $item['id_barang']; ?>">
+                                <button type="submit" name="remove_from_cart" class="remove-items btn btn-danger p-0" style="height: 1rem; width: 1rem; font-size: .6rem">x</button>
+                              </form>
+                              <div class="items-kuantiti d-flex align-items-end gap-1 ms-4 me-0">
+                                <p class="m-0"><?php echo $itemQuantity . ' item'; ?></p>
+                              </div>
+                          </div>
+                        </div>
+              <?php
+                        $totalQuantity += $itemQuantity;
+                    }
+                  }
+              ?>
+            </div>
             </div>
             <div class="bayar-container border-top py-2 px-1 mt-2">
               <div class="bayar-items d-flex justify-content-between bg-dongker py-2 px-2 rounded-4 align-items-center">
-                <div class="bayar-teks ms-2">
-                  <p class="mb-1 jml-item">4 items</p>
-                  <p class="text-white mb-0 jml-harga">Rp. 40.000</p>
-                </div>
-                <button type="submit" class="rounded-5 px-3 bg-white" style="height: 2rem" data-bs-toggle="modal" data-bs-target="#bayarModal">Entry</button>
+                  <div class="bayar-teks ms-2">
+                      <p class="mb-1 jml-item"><?php echo $totalQuantity . " items"; ?></p>
+                      <p class="text-white mb-0 jml-harga"><?php echo "Rp. " . number_format($totalPrice); ?></p>
+                  </div>
+                  <button type="submit" class="rounded-5 px-3  bg-white" style="height: 2rem" data-bs-toggle="modal" data-bs-target="#bayarModal">Entry</button>
               </div>
             </div>
           </div>
@@ -114,9 +154,35 @@
               <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Proses Transaksi</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            <form method="POST" action="admin/fungsi/checkout.php">
+                  <div class="modal-body d-flex flex-column align-items-center">
+                      <p class="mb-2">Harga Total</p>
+                      <h4 class="fw-bold"><?php echo "Rp. " . number_format($totalPrice); ?></h4>
+                  </div>
+                  <div class="modal-body">
+                      <p class="mb-1">Uang yang dibayar</p>
+                      <input type="text" class="form-control" id="uangBayar" name="uangBayar" />
+                  </div>
+                  <div class="modal-footer">
+                      <button type="submit" class="btn btn-danger" name="bayar">Bayar</button>
+                  </div>
+                  </form>
+              </div>
+        </div>
+      </div>
+
+
+
+      <!-- <div class="modal fade" id="bayarModal" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Proses Transaksi</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
             <div class="modal-body d-flex flex-column align-items-center">
               <p class="mb-2">Harga Total</p>
-              <h4 class="fw-bold">Rp. 40.000</h4>
+              <h4 class="fw-bold"><?php echo "Rp. " . number_format($totalPrice); ?></h4>
             </div>
             <div class="modal-body">
               <p class="mb-1">Uang yang dibayar</p>
@@ -127,7 +193,7 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> -->
 
       <div class="modal fade" id="strukModal" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
         <div class="modal-dialog modal-dialog-centered modal-lg">
@@ -195,6 +261,5 @@
         </div>
       </div>
     </main>
-
 
 
